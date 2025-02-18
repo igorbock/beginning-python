97a5465f8ca37e073f54fbc16e3fba45bb45bcc1
igorbock <igorbdaluz@hotmail.com>
2025-02-17 23:01:08 -0300

    Primeiro commit com os 5 primeiros exercícios da aula1. #1

---
+++ aula1.py
@@ -0,0 +1,27 @@
+#Exercício 1
+#inteiro = int(input("Digite um número inteiro: "))
+#print(f"Você digitou: {inteiro}")
+
+#Exercício 2
+#decimal = float(input("Digite um número com decimal: "))
+#print(f"Você digitou: {decimal}")
+
+#Exercício 3
+#valor1 = int(input("Digite o primero valor inteiro: "))
+#valor2 = int(input("Digite o segundo valor inteiro: "))
+#valor3 = int(input("Digite o terceiro valor inteiro: "))
+#soma = valor1 + valor2 + valor3
+
+#print(f"O resultado dos três valores somados é: {soma}")
+
+#Exercício 4
+#numero_real = float(input("Digite um número decimal para descobrir o seu quadrado: "))
+#quadrado = numero_real * numero_real
+
+#print(f"O valor do número {numero_real} ao quadrado é: {quadrado}")
+
+#Exercício 5
+#numero_real = float(input("Digite um número decimal e descubra a quinta parte desse número: "))
+#quinta_parte = numero_real / 5
+
+#print(f"A quinta parte do número {numero_real} é: {quinta_parte}")
