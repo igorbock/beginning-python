c522548dc73ff771c20b41cabf5fc66f3bdbe775
igorbdaluz <igorbdaluz@hotmail.com>
2026-02-25 23:38:38 -0300

    Exercícios do Coddy

---
+++ 02_11_funcoes_avancadas/funcoes_lambda_parte_1.py
@@ -0,0 +1,11 @@
+# Crie uma função lambda chamada multiply que recebe três argumentos, x, y, e z, e retorna o produto deles.
+# Após definir a função lambda, chame-a com os valores 2, 3, e 4, e imprima o resultado.
+
+# Create a lambda function that multiplies three numbers
+multiply = lambda x, y, z: x * y * z
+
+# Call the lambda function with the values 2, 3, and 4
+result = multiply(2, 3, 4)
+
+# Print the result
+print(result)
