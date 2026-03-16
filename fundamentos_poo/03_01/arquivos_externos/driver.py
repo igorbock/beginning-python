24de3116fbd5e31bc34628f7cf52c527c5f32c6e
igorbdaluz <igorbdaluz@hotmail.com>
2026-03-15 21:43:08 -0300

    Subindo os primeiros exercícios de POO do python pelo Coddy.

---
+++ fundamentos_poo/03_01/arquivos_externos/driver.py
@@ -0,0 +1,9 @@
+# Você recebeu os arquivos Python (my_class.py e driver.py), adicione uma instrução de importação no arquivo driver.py
+# para importar a classe MyClass pelo nome de my_class.py usando a sintaxe: from my_class import MyClass
+
+# TODO: update the import
+from my_class import MyClass
+
+# Test code here
+test_case = input()
+print('Test completed')
---
+++ fundamentos_poo/03_01/arquivos_externos/my_class.py
@@ -0,0 +1,6 @@
+# Você recebeu os arquivos Python (my_class.py e driver.py), adicione uma instrução de importação no arquivo driver.py
+# para importar a classe MyClass pelo nome de my_class.py usando a sintaxe: from my_class import MyClass
+
+class MyClass:
+    def __init__(self):
+        pass
