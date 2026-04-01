20dbbddb2dd9d67bd45c063fa26c3ba076962fd8
igorbdaluz <igorbdaluz@hotmail.com>
2026-03-31 23:15:40 -0300

    aula de python pelo Coddy

---
+++ fundamentos_poo/03_05/05_resumo_calculadora_formas/rectangle.py
@@ -0,0 +1,25 @@
+# TODO: Import the Shape class from shape.py
+# from shape import Shape
+from shape import Shape
+
+class Rectangle(Shape):
+    def __init__(self, width, height):
+        # TODO: Initialize the Rectangle with the given width and height
+        # Store width and height as instance attributes
+        self.width = width
+        self.height = height
+    
+    def area(self):
+        # TODO: Implement the area method for a rectangle
+        # Formula: width * height
+        return self.width * self.height
+    
+    def perimeter(self):
+        # TODO: Implement the perimeter method for a rectangle
+        # Formula: 2 * (width + height)
+        return 2 * (self.width + self.height)
+    
+    def __str__(self):
+        # TODO: Override the __str__ method to return a description
+        # The description should be exactly: "Rectangle with width [width] and height [height]"
+        return f"Rectangle with width {self.width} and height {self.height}"
