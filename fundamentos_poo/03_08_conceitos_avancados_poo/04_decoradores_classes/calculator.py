854b8f83b37d2b7cf03001edcc4ddf6d16f9c95e
igorbdaluz <igorbdaluz@hotmail.com>
2026-04-13 00:10:22 -0300

    Subindo exercícios do Python

---
+++ fundamentos_poo/03_08_conceitos_avancados_poo/04_decoradores_classes/calculator.py
@@ -0,0 +1,16 @@
+# TODO: Import the add_counter decorator from decorator.py
+# Use format: from decorator import add_counter
+
+# TODO: Apply the add_counter decorator to the Calculator class
+# Use the @add_counter syntax above the class definition
+class Calculator:
+    def __init__(self):
+        pass
+        
+    def add(self, a, b):
+        # TODO: Implement the add method that returns the sum of a and b
+        pass
+        
+    def subtract(self, a, b):
+        # TODO: Implement the subtract method that returns the difference of a and b
+        pass
