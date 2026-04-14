f331ba16c81fd794d075134e70021c21d2b1999c
igorbdaluz <igorbdaluz@hotmail.com>
2026-04-14 20:40:49 -0300

    Exercícios de Pyhton

--- fundamentos_poo/03_08_conceitos_avancados_poo/04_decoradores_classes/decorator.py
+++ fundamentos_poo/03_08_conceitos_avancados_poo/04_decoradores_classes/decorator.py
@@ -1,33 +1,22 @@
 def add_counter(cls):
-    # TODO: Initialize the call_counts dictionary with keys "add" and "subtract" both starting at 0
+    # Inicializa os contadores na classe
     cls.call_counts = {"add": 0, "subtract": 0}
-    
-    # TODO: Store original methods
-    # Store the original add method from the class
-    original_add = None  # Replace with actual code
-    
-    # Store the original subtract method from the class
-    original_subtract = None  # Replace with actual code
-    
-    # TODO: Define wrapped methods that increment counters and preserve original functionality
+
+    # Guarda os métodos originais
+    original_add = cls.add
+    original_subtract = cls.subtract
+
+    # Define wrappers que incrementam contadores e chamam o original
     def wrapped_add(self, a, b):
-        # TODO: Increment the "add" counter in call_counts
-        # HINT: Use self.__class__.call_counts to access the class attribute
-        
-        # TODO: Call the original add method and return its result
-        pass
-    
+        self.__class__.call_counts["add"] += 1
+        return original_add(self, a, b)
+
     def wrapped_subtract(self, a, b):
-        # TODO: Increment the "subtract" counter in call_counts
-        # HINT: Use self.__class__.call_counts to access the class attribute
-        
-        # TODO: Call the original subtract method and return its result
-        pass
-    
-    # TODO: Replace original methods with wrapped versions
-    # Replace the add method with wrapped_add
-    
-    # Replace the subtract method with wrapped_subtract
-    
-    # TODO: Return the modified class
+        self.__class__.call_counts["subtract"] += 1
+        return original_subtract(self, a, b)
+
+    # Substitui os métodos na classe
+    cls.add = wrapped_add
+    cls.subtract = wrapped_subtract
+
     return cls
